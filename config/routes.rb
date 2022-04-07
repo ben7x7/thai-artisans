990e9d1f9ae5d8e292abdc1a7314303902de71b7
benoit bessiere <benoit.bessiere@gmail.com>
2022-04-07 16:58:27 +0200

    set I18n + add new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    get 'contact', to: 'pages#contact', as: :contact
+    get 'kookies', to: 'pages#kookies', as: :kookies
+    get 'mentions', to: 'pages#mentions', as: :mentions
+    get 'menu', to: 'pages#menu', as: :menu
+    get 'rgpd', to: 'pages#rgpd', as: :rgpd
+    resources :reservations, only: [:new, :create]
+  end
 end
