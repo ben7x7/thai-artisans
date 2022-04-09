beb235f97d1ff63fbb8c5d11cdbe5e91e9e291bc
benoit bessiere <benoit.bessiere@gmail.com>
2022-04-09 08:10:49 +0200

    add contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def contact
+  end
+
+  def menu
+  end
+
+  def kookies
+  end
+
+  def mentions
+  end
+
+  def rgpd
+  end
 end
