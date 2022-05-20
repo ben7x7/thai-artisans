012fa57160c2fa98db8b1ff6905c47e81e8d1c51
benoit bessiere <benoit.bessiere@gmail.com>
2022-05-20 17:53:33 +0200

    adjust ReservationMailer syntax error

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,8 +2,9 @@ class ReservationMailer < ApplicationMailer
 
   def send_reservation_mail(booking)
     @booking = booking
-    mail( :to => contact@thai-artisans.ch,
-          :from => @booking.email,
-          :subject => 'Thai-Artisans Reservation')
+    mail(to: 'contact@thai-artisans.ch',
+        from: @booking.email,
+        subject: 'Thai-Artisans Reservation')
   end
+
 end
