a60a70d1983e896763913a7db7729ae7de132171
benoit bessiere <benoit.bessiere@gmail.com>
2022-05-21 06:52:00 +0200

    change the from: email

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,7 +3,7 @@ class ReservationMailer < ApplicationMailer
   def send_reservation_mail(booking)
     @booking = booking
     mail(to: 'contact@thai-artisans.ch',
-        from: @booking.email,
+        from: 'contact@thai-artisans.ch',
         subject: 'Thai-Artisans Reservation')
   end
 
