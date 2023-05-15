2b56959f7f992010a5fa706e0f06e212f8c4ba0a
benoit bessiere <benoit.bessiere@gmail.com>
2023-05-15 05:21:51 +0200

    db:create and mmigrate"

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,7 +2,7 @@ class ReservationMailer < ApplicationMailer
 
   def send_reservation_mail(booking)
     @booking = booking
-    mail(to: 'contact@thai-artisans.ch',
+    mail(to: 'benoit.bessiere@gmail.com',
         from: 'contact@thai-artisans.ch',
         subject: 'Thai-Artisans Reservation')
   end
