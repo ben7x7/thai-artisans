ea691eff5aedde836c25bfe652e663ed88e382ac
benoit bessiere <benoit.bessiere@gmail.com>
2023-05-15 06:13:10 +0200

    add cc to reservation booking

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,6 +3,7 @@ class ReservationMailer < ApplicationMailer
   def send_reservation_mail(booking)
     @booking = booking
     mail(to: 'benoit.bessiere@me.com',
+        cc: 'benoit.bessiere@gmail.com',
         from: 'contact@thai-artisans.ch',
         subject: 'Thai-Artisans Reservation')
   end
